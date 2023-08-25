c5e03206a8bb13f6a6b41768feb2989a7213e7c5
lucas-leoni <lucas.leoni09@gmail.com>
2023-08-24 21:39:19 -0300

    update

---
+++ test/index.test.js
@@ -0,0 +1,19 @@
+import { describe, expect, it } from '@jest/globals';
+import { somar } from '../exercicios/um';
+
+describe('Testes do primeiro exercício', () => {
+  // Executado antes de TODOS os testes
+  beforeAll(async () => {
+    console.info('Iniciando TDD com jest!');
+  });
+
+  // Executado após TODOS os testes
+  afterAll(() => {
+    console.info('Encerrados os testes');
+  });
+
+  it('Should sum two numbers', () => {
+    const result = somar(1, 2);
+    expect(result).toBe(3);
+  });
+});
