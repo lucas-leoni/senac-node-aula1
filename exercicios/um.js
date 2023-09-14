65cb1ea7e686f54cf971018c16ec89060297429b
lucas-leoni <lucas.leoni09@gmail.com>
2023-09-14 19:34:17 -0300

    try and catch added

--- exercicios/um.js
+++ exercicios/um.js
@@ -2,15 +2,23 @@
 
 function run(app) {
   app.post('/api/exercicio1', (req, res) => {
-    const result = somar(req.body.num1, req.body.num2);
-
-    res.status(200).json({
-      message: `Resultado: ${result}`,
-    });
+    try {
+      const result = somar(req.body.num1, req.body.num2);
+      res.status(200).json({
+        message: `Resultado: ${result}`,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: 'Deu ruim' });
+    }
   });
 }
 
 function somar(num1, num2) {
+  if (isNaN(num1) || isNaN(num2)) {
+    throw new Error('Favor informar números');
+  }
+
   return num1 + num2;
 }
 
